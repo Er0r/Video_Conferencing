b400a1a254ab98fd75cb5ff4fa3901dd7ad5058f
Er0r <fahimmaria155@gmail.com>
2020-12-03 20:09:34 +0600

    Developing React Router

---
+++ client/src/homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+function Homepage() {
+    return (
+        <div>
+            <h1> Hello, Welcome</h1>
+        </div>
+    )
+    
+}
+
+export default Homepage;
